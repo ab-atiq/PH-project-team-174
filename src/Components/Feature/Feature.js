2c0b62e90809f391650014594313e05764d9369c
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-02-17 17:54:27 +0600

    some bug fixed

--- src/Components/Feature/Feature.js
+++ src/Components/Feature/Feature.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Feature = () => {
     return (
         <div>
-            <h2>This is Feature page </h2>
+            <h1>This is Feature page </h1>
         </div>
     );
 };
